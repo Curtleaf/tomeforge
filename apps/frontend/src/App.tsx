bc41ffac0c420db07d264c7d12af814f38058110
CurtLeaf <curtLeaf@gmail.com>
2025-01-11 18:17:03 -0600

    Added base for frontend and a concurrent loader

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to TomeForge Frontend</h1>
+        <button >Click Me</button>
+      </header>
+    </div>
+  );
+};
+
+export default App;
